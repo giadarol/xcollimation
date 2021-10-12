59ed2a7e1411c81f1d839b92dbce4510e9ed8dfc
Frederik Van der Veken <frederik@cern.ch>
2021-10-12 16:41:47 +0200

    Lossmap :-P

--- run.py
+++ run.py
@@ -4,10 +4,14 @@ import pandas as pd
 import xtrack as xt
 import xline as xl
 
+import logging
+logger = logging.getLogger('xtrack')
+logger.setLevel(logging.DEBUG)
+
 df = pd.read_csv('InputDist.csv')
 
-n_part = 2000
-n_turns = 200
+n_part = 20000
+n_turns = 10
 
 part_input = xt.Particles(
     p0c=7000e9,
@@ -56,8 +60,40 @@ tracker.track(part, ele_start=i_start_track)
 part.at_element[part.state==0]
 
 for i_turn in range(n_turns):
+    part.s[part.state>0] = 0.
     tracker.track(part)
     n_alive = ((part.state)>0).sum()
     print(f'At turn: {i_turn} {n_alive=}')
     if n_alive<1:
         break
+
+part_before_refinement = part.copy()
+
+loss_loc_refinement = xt.LossLocationRefinement(tracker,
+                                            n_theta = 360,
+                                            r_max = 1., # m
+                                            dr = 50e-6,
+                                            ds = 0.1,
+                                            save_refine_trackers=False)
+
+import time
+t0 = time.time()
+
+loss_loc_refinement.refine_loss_location(part)
+
+t1 = time.time()
+print(f'Took\t{(t1-t0)*1e3:.2f} ms')
+
+mask_lost = part.state<1
+
+hist, bin_edges = np.histogram(part.s[mask_lost], range=(0, sequence.get_length()), bins=27000)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+
+plt.figure(1)
+plt.semilogy(bin_edges[:-1], hist, '.')
+for ii in range(9):
+    plt.axvline(x=l/8*ii)
+
+plt.show()
